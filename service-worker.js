9ae897261bb72487f27a5edc03d0159645c16755
eric <deadfish892004@gmail.com>
2022-01-31 21:40:18 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gamerswebsitefinal/precache-manifest.7fdaaec8a7f9443e2f03390acf798c48.js"
+  "/gamerswebsitefinal/precache-manifest.ed7bd753fa746d3cbc94948c023cc487.js"
 );
 
 self.addEventListener('message', (event) => {
